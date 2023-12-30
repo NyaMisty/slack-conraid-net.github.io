6731bebe603acea149347af64c1ee71ec912b60b
Sakuragawa Misty <gyc990326@gmail.com>
2023-12-30 10:17:59 +0800

    slack.conraid.net repo export 20231230

---
+++ privacy.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Repository to Slackware64 Current by Conraid">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Conraid Repository</title>
+<meta property="og:site_name" content="conraid">
+<meta property="og:locale" content="it_IT">
+<meta property="og:url" content="http://slack.conraid.net">
+<meta property="og:title" content='Slackers'>
+<meta property="og:description" content="Visita il sito di Corrado Franco">
+<meta property="og:image" content="http://slack.conraid.net/images/avatar.jpg">
+<meta property="og:type" content="aboutme_prod:page">
+<meta property="fb:app_id" content="568492989985060">
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@Slackers_it">
+<meta name="twitter:title" content='Slackers'>
+<meta name="twitter:description" content="Visita il sito di Corrado Franco">
+<meta name="twitter:image" content="http://slack.conraid.net/images/avatar.jpg">
+<meta name="twitter:image:width" content="520">
+<meta name="twitter:image:height" content="520">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">  
+<link rel="canonical" href="http://slack.conraid.net/privacy.php">
+</head>
+<body>
+<header>
+<h1>Repository for Slackware64 current</h1>
+</header>
+
+<article>
+<div  style="text-align: center;">
+<h2>Privacy Policy</h2>
+<h3>INFORMATIVA ai sensi del REGOLAMENTO EUROPEO n. 679/2016 </h3>
+<p>(Regolamento generale sulla protezione dei dati; in inglese: General Data Protection Regulation – GDPR)</p>
+</div>
+<p>In questa pagina si descrivono le modalit&#224; di gestione del sito in riferimento al trattamento dei dati personali degli utenti che lo consultano.</p>
+<p>Si tratta di un'informativa per i dati accessibili per via telematica a partire dagli indirizzi:</p>
+<p><strong>http://slack.conraid.net</strong></p>
+<p>corrispondenti alla pagina iniziale di questo sito (anche se con dominio diverso fanno riferimento agli stessi file).</p>
+<p>L'informativa &#232; resa solo per questo sito e non anche per altri siti web eventualmente consultati dall'utente tramite link.</p>
+<h3>Dati di navigazione</h3>
+<p>I sistemi informatici e le procedure software preposte al funzionamento di questo sito web acquisiscono, nel corso del loro normale esercizio, alcuni dati relativi alla connessione al sito e la cui trasmissione è implicita nell'uso dei protocolli di comunicazione di Internet. Si tratta di informazioni che non sono raccolte per essere associate a interessati identificati, ma che per loro stessa natura potrebbero, attraverso elaborazioni e associazioni con dati detenuti da terzi, permettere di identificare gli utenti suddetti.</p>
+<p>In questa categoria di dati rientrano gli indirizzi IP o i nomi a dominio dei computer utilizzati dagli utenti che si connettono al sito, gli indirizzi in notazione URI (Uniform Resource Identifier) delle risorse richieste, l'orario della richiesta, il metodo utilizzato nel sottoporre la richiesta al server, la dimensione del file ottenuto in risposta, il codice numerico indicante lo stato della risposta data dal server (buon fine, errore, ecc.) e altri parametri relativi al sistema operativo e all'ambiente informatico dell'utente.</p>
+<p>Questi dati vengono utilizzati al solo fine di ricavare informazioni statistiche anonime sull'uso del sito e per controllare il corretto funzionamento di questo. I dati potrebbero essere utilizzati per l'accertamento di responsabilità in caso di reati informatici ai danni del sito.</p>
+<h3>Trattamento dei dati personali</h3>
+<p>I dati personali raccolti attraverso eventuali <i>form</i> saranno trattati non saranno memorizzati nel server, nel sito o nel database, ma solo gestiti lato client dal titolare del trattamento, e usati solo per lo scopo di rispondere alla richiesta. Il conferimento di tali dati ha sempre carattere facoltativo, salva la necessit&#224; dell'indicazione di alcuni elementi necessari per l'erogazione dell'eventuale servizio richiesto.</p>
+<p>I dati forniti tramite form non verrano utilizzati in altri contesti, né saranno forniti a terze persone per scopi diversi da quanto indicato sopra. In relazione al trattamento dei dati l'utente potr&#224; esercitare i diritti previsti dall'art. 7 del d.lgs. 196/2003 e dal REGOLAMENTO EUROPEO n. 679/2016.</p>
+<h3>Cookie</h3>
+<p>Questo sito non utilizza alcun cookie, nemmeno per fini statistici.</p>
+<h3>Diritti dell'interessato</h3>
+<p>L’interessato potrà, in qualsiasi momento, esercitare i diritti di cui agli artt. 15-22 del GDPR, ovvero:</p>
+<p>
+- Diritto di accesso ai dati personali (conferma del trattamento, accesso ai dati e diritto ad una copia);<br>
+- Diritto di rettifica (correzione o integrazione dei dati personali inesatti);<br>
+- Diritto all’oblio (diritto di decidere che siano cancellati e non sottoposti ulteriormente a trattamento i dati personali non più necessari per le finalità per le quali sono stati raccolti);<br>
+- Diritto di limitazione del trattamento (limitazione alla sola conservazione dei dati);<br>
+- Diritto di reclamo all’autorità di controllo (Garante della Privacy) per ogni presunta violazione del Regolamento;<br>
+- Diritto alla portabilità (ricezione dei dati personali in un formato strutturato, di uso comune e leggibile da dispositivo automatico e trasmissione ad altro titolare senza impedimenti);<br>
+- Diritto di opposizione in qualsiasi momento al trattamento.<br>
+- Diritto di revocare il consenso, ove previsto.<br>
+</p>
+<p>Per una esaustiva visione dei diritti dell'interessato leggere le <a href="https://www.garanteprivacy.it/regolamentoue/diritti-degli-interessati" target="_blank" title="Diritti degli interessati nel sito del Garante della Privacy">pagine del Garante della Privacy</a>.</p>
+<h3>Il titolare del trattamento</h3>
+<p>Il titolare del trattamento dei dati &#232; Corrado Franco.</p>
+<p>I dati saranno conservati per il tempo prescritto dalle norme di legge.</p>
+</article>
+<footer style="text-align: center;" ><p><a href="index.php">Torna alla home</a></p></footer>
+</div>
+<div class="supportme">
+<a href="https://paypal.me/conraid" title="Sostieni questo repository">
+<i>Support</i><i>Me</i>
+</a>
+</div>
+<script defer type="text/javascript">
+var speed = document.createElement('link');
+speed.rel = 'stylesheet';
+speed.href = 'css/slackers.min.css';
+speed.type = 'text/css';
+var defer = document.getElementsByTagName('link')[0];
+defer.parentNode.insertBefore(speed, defer);
+</script>
+<noscript>
+<link rel="stylesheet" type="text/css" href="css/slackers.min.css">
+</noscript>
+<a rel="me" href="https://mastodon.social/@conraid">Mastodon</a>
+</body>
+</html>
